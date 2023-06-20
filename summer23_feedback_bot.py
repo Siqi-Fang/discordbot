0985e1bbb2a6727c7c8909a36d20921dd0f9e801
Siqi-Fang <sfang@hamilton.edu>
2023-06-20 16:35:35 -0400

    first commit

---
+++ summer23_feedback_bot.py
@@ -0,0 +1,192 @@
+import discord
+from discord.ext import commands, tasks
+from discord.utils import get
+import logging
+from datetime import datetime, timedelta
+from asgiref.sync import sync_to_async
+import pytz
+from utils import MetabaseService, NAME_2_TRACK, TRACKS
+import pandas as pd
+import os
+
+@sync_to_async
+def metabase():
+    '''
+    Creates Metabase service to recieve data.
+    '''
+    return MetabaseService()
+
+
+async def get_summer_overall_data():
+    '''
+    Gets student rating for the entire program every day from Metabase
+    '''
+    daily_data = {}
+    mb = await metabase()
+
+    todays_reviews = 0
+    yesterdays_reviews = 0
+
+    today = await sync_to_async(mb.retrieve)(638)
+    yesterday = await sync_to_async(mb.retrieve)(643)
+    df_class_student = await sync_to_async(mb.retrieve)(640)
+
+    df_today = pd.merge(today, df_class_student,
+                  on='discordId', how='inner')
+    df_yest = pd.merge(yesterday, df_class_student,
+                       on='discordId', how='inner')
+
+    yesterday_data = round(df_yest['rating'].mean(), 2)
+    today_data = round(df_today['rating'].mean(), 2)
+
+    # daily avg 
+    avg_percent_change = round(
+        (-(yesterday_data - today_data) / yesterday_data) * 100, 2)
+    change_message = str(today_data) + '\nChange Since Yesterday: ' + \
+        str(avg_percent_change) + '%'
+    daily_data['Today\'s Average Rating'] = change_message
+
+    #  review count
+    todays_reviews = df_today['studentUsername'].count()
+    yesterdays_reviews = df_yest['studentUsername'].count()
+    count_percent_change = round(
+        (-(yesterdays_reviews - todays_reviews) / yesterdays_reviews) * 100, 2)
+    count_message = str(todays_reviews) + \
+        '\nChange Since Yesterday: ' + str(count_percent_change) + '%'
+    daily_data['Today\'s Number of Reviews'] = count_message
+
+    # high ratings
+    today_high = df_today[df_today['rating'] >= 9]['rating'].count()
+    yesterday_high = df_yest[df_yest['rating'] >= 9]['rating'].count()
+
+    high_percent_change = round(
+        (-(yesterday_high - today_high) / yesterday_high) * 100, 2)
+    high_message = str(today_high) + '\nChange Since Yesterday: ' + \
+        str(high_percent_change) + '%'
+    daily_data['Today\'s Percentage of 10 and 9 Reviews'] = high_message
+
+    # low ratings
+    today_low = df_today[df_today['rating'] <= 7]['rating'].count()
+    yesterday_low = df_yest[df_yest['rating'] <= 7]['rating'].count()
+
+    low_percent_change = round(
+        (-(yesterday_low - today_low) / yesterday_low) * 100, 2)
+    low_message = str(today_low) + '\nChange Since Yesterday: ' + \
+        str(low_percent_change) + '%'
+    daily_data['Today\'s Percentage of 7 or Less Reviews'] = low_message
+
+    return daily_data
+
+
+async def get_summer_group_data():
+    '''
+    Gets student rating for each group every day from Metabase
+    '''
+    daily_data = {}
+    mb = await metabase()
+
+    today = await sync_to_async(mb.retrieve)(638)
+    df_class_student = await sync_to_async(mb.retrieve)(640)
+
+    df = pd.merge(today, df_class_student,
+                        on='discordId', how='inner')
+    df['track'] = df['name'].map(NAME_2_TRACK)
+    df = df[df['track'].notna()]
+
+    for track in TRACKS:
+        daily_data[track + ' Average Rating Today'] = round(
+            df[df["track"]==track]["rating"].mean(), 2)
+        
+    return daily_data
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+@bot.command(name='groupreport')
+async def get_summer_group_report(ctx):
+    embed = discord.Embed()
+
+    data = await get_summer_group_data()
+
+    embed.title = 'Summer Group Ratings Report'
+    embed.color = discord.Color.green()
+
+    for key, value in data.items():
+        embed.add_field(name=key, value=value, inline=False)
+
+    return await ctx.send(embed=embed)
+
+
+@bot.command(name='overallreport')
+async def get_summer_overall_report(ctx):
+    embed = discord.Embed()
+
+    data = await get_summer_overall_data()
+
+    embed.title = 'Summer Group Ratings Report'
+    embed.color = discord.Color.green()
+
+    for key, value in data.items():
+        embed.add_field(name=key, value=value, inline=False)
+
+    return await ctx.send(embed=embed)
+
+@tasks.loop(hours=1)
+async def auto_summer_group_report():
+    '''
+    Requests metabase data and automatically sends it at the end of the week
+    '''
+    date_utc = datetime.now(tz=pytz.UTC)
+    date_pst = date_utc.astimezone(pytz.timezone('US/Pacific'))
+
+    if date_pst.hour == 14: # CHANGE BACK TO 16 TO RE-ENABLE
+
+        embed = discord.Embed()
+
+        data = await get_summer_group_data()
+
+        channel = discord.utils.get(bot.get_all_channels(), name='summer-camp')
+
+        embed.title = ''
+        embed.description = ''
+        embed.color = discord.Color.green()
+
+        for key, value in data.items():
+            embed.add_field(name=key, value=value, inline=False)
+
+        return await channel.send(embed=embed)
+
+@tasks.loop(hours=1)
+async def auto_summer_overall_report():
+    '''
+    Requests metabase data and automatically sends it at the end of the week
+    '''
+    date_utc = datetime.now(tz=pytz.UTC)
+    date_pst = date_utc.astimezone(pytz.timezone('US/Pacific'))
+
+    if date_pst.hour == 14: # CHANGE BACK TO 16 TO RE-ENABLE
+
+        embed = discord.Embed()
+
+        data = await get_summer_overall_data()
+
+        channel = discord.utils.get(bot.get_all_channels(), name='summer-camp')
+
+        embed.title = ''
+        embed.description = ''
+        embed.color = discord.Color.green()
+
+        for key, value in data.items():
+            embed.add_field(name=key, value=value, inline=False)
+
+        return await channel.send(embed=embed)
+    
+@bot.event
+async def on_ready():
+    auto_summer_group_report.start()
+    auto_summer_overall_report.start()
+
+bot.run(os.getenv("TOKEN"))
---
+++ utils.py
@@ -0,0 +1,122 @@
+import requests
+import logging
+import pandas as pd
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+EMAIL = os.getenv("EMAIL")
+PASSWORD = os.getenv("PASSWORD")
+
+NAME_2_TRACK = {
+    "pixel-penguins": "One Week DS (EST)",
+    "algorithmic-armadillos": "One Week DS (PST)",
+    "cyborg-cheetahs": "One Week GameDev (PST)",
+    "galactic-goblins": "3-Week NLP (EST)",
+    "crystal-cyclones": "3-Week NLP (EST)",
+    "delta-droids": "3-Week NLP (EST)",
+    "energy-emperors": "3-Week NLP (EST)",
+    "binary-breakers": "3-Week NLP (EST)",
+    "twilight-titans": "3-Week NLP (EST)",
+    "lunar-lasers": "3-Week NLP (EST)",
+    "turbo-tornadoes": "3-Week CV (EST)",
+    "vector-vipers": "3-Week CV (EST)",
+    "diamond-dragons": "3-Week CV (EST)",
+    "comet-crusaders" :"3-Week CV (EST)",
+    "mystic-moons": "3-Week CV (EST)",
+    "electron-elephants": "3-Week DS (EST)",
+    "kepler-koalas": "3-Week DS (EST)",
+    "solar-squirrels": "3-Week DS (EST)",
+
+    "web-wolves": "One Week DS (EST)",
+    "protocol-parrots": "One Week DS (EST)",
+    "network-nighthawks": "One Week GameDev (EST)",
+    "server-snakes": "One Week GameDev (EST)",
+
+    "silicon-starlings": "One Week DS (PST)",
+    "firewall-flamingos": "One Week DS (PST)",
+    "overclock-owls": "One Week GameDev (PST)",
+    "photon-phoenixes": "3-Week NLP (PST)",
+    "helix-hawks": "3-Week NLP (PST)",
+    "matrix-mustangs": "3-Week NLP (PST)",
+    "fusion-flamingos": "3-Week NLP (PST)",
+    "omega-otters": "3-Week NLP (PST)",
+    "cyberspace-cheetahs": "3-Week CV (PST)",
+    "jetstream-jaguars": "3-Week CV (PST)",
+    "vortex-vultures": "3-Week CV (PST)",
+    "data-dolphins": "3-Week CV (PST)",
+    "lumen-lemurs": "3-Week CV (PST)",
+    "spectrum-stingrays": "3-Week DS (PST)",
+    "kilobit-koalas": "3-Week DS (PST)",
+    "radiant-ravens": "3-Week DS (PST)",
+    "fractal-firebirds": "3-Week DS (PST)",
+    "agile-antelopes": "One Week DS (EST)",
+    "brawny-beavers": "One Week DS (EST)",
+    "crimson-crocodiles": "One Week GameDev (EST)", 
+    "dynamic-dolphins": "One Week DS (PST)",
+}
+
+TRACKS = ["One Week DS (EST)",
+          "One Week DS (PST)",
+          "One Week GameDev (EST)",
+          "One Week GameDev (PST)",
+          "3-Week DS (EST)",
+          "3-Week DS (PST)",
+          "3-Week CV (EST)",
+          "3-Week CV (PST)",
+          "3-Week NLP (EST)",
+          "3-Week DS (PST)",
+          ]
+
+class MetabaseService:
+    '''
+    Generic Metabase service class
+    '''
+
+    def __init__(self):
+        self.session = requests.Session()
+        self.logger = logging.getLogger('discord-bot')
+
+    def retrieve_data(self, response):
+        request_data_response = response
+        request_data = request_data_response.json().get('data')
+        request_rows = request_data.get('rows')
+
+        request_columns_raw = request_data.get('cols')
+        request_columns = []
+        for request_column in request_columns_raw:
+            request_columns.append(request_column.get('display_name'))
+
+        request_database = pd.DataFrame(request_rows, columns=request_columns)
+
+        return request_database
+
+    def retrieve(self, id):
+        """id is the number in the url of the metabase question you created
+           ..../question/<id>/your-question-name
+           returns a pandas dataframe
+        """
+        self.login()
+        request_data_response = self.session.post(
+            f'https://data.ai-camp.dev/api/card/{id}/query')
+        request_database = self.retrieve_data(request_data_response)
+        return request_database
+
+    def login(self):
+        '''
+        Logs into metabase and stays logged in while the session is alive
+        '''
+        json = {
+            'username': EMAIL,
+            'password': PASSWORD 
+        }
+        response = self.session.post(
+            'https://data.ai-camp.dev/api/session', json=json)
+        if response.status_code == 200:
+            token = response.json().get('id')
+            self.logger.info(
+                'Metabase login successful, new token created and saved')
+        else:
+            self.logger.error(f'Metabase login failed: {response.text}')
+
